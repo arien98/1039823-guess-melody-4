11d251714026cbebb9c24894d69943d2a3bea9f3
Светлана Хамматова <sveta.wing@gmail.com>
2020-06-09 20:50:05 +0500

    Добавляет вебпак

--- src/index.js
+++ src/index.js
@@ -1,4 +1,2 @@
 const bodyElement = document.querySelector(`body`);
-const p = createElement(`p`);
-p.textContent = `Hello!`;
-bodyElement.append(p);
+bodyElement.textContent = `Hello, hello, hello!`;
