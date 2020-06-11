b81ba9905b4aab6897e13e7e22494de90b1e1e88
Светлана Хамматова <sveta.wing@gmail.com>
2020-06-11 18:15:55 +0500

    Добавляет приветственный компонент

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app/app.jsx";
+
+const Settings = {
+  ERRORS_COUNT: 3,
+};
+
+const init = () => {
+  ReactDOM.render(
+      <App
+        errorsCount = {Settings.ERRORS_COUNT}
+      />,
+      document.querySelector(`#root`)
+  );
+};
+
+init();
